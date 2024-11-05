d97b20c7b781387805c3e7906f9f19139ad024b3
alientim <tim@eertmoed.de>
2024-11-05 13:38:46 +0100

    Update GGDL_GUI.ps1

--- GGDL_GUI.ps1
+++ GGDL_GUI.ps1
@@ -2,7 +2,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 Import-Module ActiveDirectory
 
-# Funktion zum Abrufen der aktuellen Domäne
+# Funktion zum Abrufen der aktuellen DomÃ¤ne
 function Get-DomainName {
     $domain = (Get-ADDomain).DNSRoot
     return $domain
@@ -20,25 +20,25 @@ $dlList = Get-Groups
 # GUI zur Zuordnung von GG zu DL erstellen
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "GG zu DL Zuordnung"
-$form.Size = New-Object System.Drawing.Size(800, 600)  # Großes komfortables Fenster
+$form.Size = New-Object System.Drawing.Size(550, 400)  # GroÃŸes komfortables Fenster
 
 # GG-Eingabe und Liste
 $ggLabel = New-Object System.Windows.Forms.Label
 $ggLabel.Text = "Globalgruppe (GG):"
 $ggLabel.Location = New-Object System.Drawing.Point(10, 10)
-$ggLabel.Size = New-Object System.Drawing.Size(150, 20)
+$ggLabel.Size = New-Object System.Drawing.Size(250, 20)
 $form.Controls.Add($ggLabel)
 
 $ggTextBox = New-Object System.Windows.Forms.TextBox
-$ggTextBox.Location = New-Object System.Drawing.Point(160, 10)
-$ggTextBox.Size = New-Object System.Drawing.Size(300, 20)
+$ggTextBox.Location = New-Object System.Drawing.Point(10, 30)
+$ggTextBox.Size = New-Object System.Drawing.Size(250, 20)
 $form.Controls.Add($ggTextBox)
 
 $ggListBox = New-Object System.Windows.Forms.ListBox
-$ggListBox.Location = New-Object System.Drawing.Point(160, 40)
-$ggListBox.Size = New-Object System.Drawing.Size(300, 200)
+$ggListBox.Location = New-Object System.Drawing.Point(10, 55)
+$ggListBox.Size = New-Object System.Drawing.Size(250, 200)
 $ggListBox.Items.AddRange($ggList)
-$ggListBox.SelectionMode = "Single"
+$ggListBox.SelectionMode = [System.Windows.Forms.SelectionMode]::One  # Corrected this line
 $form.Controls.Add($ggListBox)
 
 $ggTextBox.Add_TextChanged({
@@ -50,20 +50,20 @@ $ggTextBox.Add_TextChanged({
 # DL-Eingabe und Liste
 $dlLabel = New-Object System.Windows.Forms.Label
 $dlLabel.Text = "Domänenlokale Gruppen (DL):"
-$dlLabel.Location = New-Object System.Drawing.Point(470, 10)
-$dlLabel.Size = New-Object System.Drawing.Size(150, 20)
+$dlLabel.Location = New-Object System.Drawing.Point(270, 10)
+$dlLabel.Size = New-Object System.Drawing.Size(250, 20)
 $form.Controls.Add($dlLabel)
 
 $dlTextBox = New-Object System.Windows.Forms.TextBox
-$dlTextBox.Location = New-Object System.Drawing.Point(630, 10)
-$dlTextBox.Size = New-Object System.Drawing.Size(300, 20)
+$dlTextBox.Location = New-Object System.Drawing.Point(270, 30)
+$dlTextBox.Size = New-Object System.Drawing.Size(250, 20)
 $form.Controls.Add($dlTextBox)
 
 $dlListBox = New-Object System.Windows.Forms.ListBox
-$dlListBox.Location = New-Object System.Drawing.Point(630, 40)
-$dlListBox.Size = New-Object System.Drawing.Size(300, 200)
+$dlListBox.Location = New-Object System.Drawing.Point(270, 55)
+$dlListBox.Size = New-Object System.Drawing.Size(250, 200)
 $dlListBox.Items.AddRange($dlList)
-$dlListBox.SelectionMode = "MultiExtended"
+$dlListBox.SelectionMode = [System.Windows.Forms.SelectionMode]::MultiExtended  # This is correct for multiple selections
 $form.Controls.Add($dlListBox)
 
 $dlTextBox.Add_TextChanged({
@@ -72,10 +72,11 @@ $dlTextBox.Add_TextChanged({
     $dlListBox.Items.AddRange($filteredDLs)
 })
 
+
 # OK-Button
 $okButton = New-Object System.Windows.Forms.Button
 $okButton.Text = "OK"
-$okButton.Location = New-Object System.Drawing.Point(200, 250)
+$okButton.Location = New-Object System.Drawing.Point(80, 300)
 $okButton.Size = New-Object System.Drawing.Size(100, 30)
 $okButton.Add_Click({
     $gg = $ggListBox.SelectedItem
@@ -83,7 +84,7 @@ $okButton.Add_Click({
 
     foreach ($dlGroup in $dl) {
         if (-not (Get-ADGroup -Filter "Name -eq '$dlGroup'")) {
-            New-ADGroup -Name $dlGroup -GroupScope DomainLocal -GroupCategory Security -Path "OU=DL_Groups,DC=deineDomäne,DC=local"
+            New-ADGroup -Name $dlGroup -GroupScope DomainLocal -GroupCategory Security -Path "OU=DL_Groups,DC=deineDomÃ¤ne,DC=local"
             Write-Output "Gruppe $dlGroup wurde erstellt."
         } else {
             Write-Output "Gruppe $dlGroup existiert bereits."
@@ -91,20 +92,20 @@ $okButton.Add_Click({
 
         if (-not (Get-ADGroupMember -Identity $dlGroup -Recursive | Where-Object { $_.SamAccountName -eq $gg })) {
             Add-ADGroupMember -Identity $dlGroup -Members $gg
-            Write-Output "Globalgruppe $gg wurde zur Domänenlokalen Gruppe $dlGroup hinzugefügt."
+            Write-Output "Globalgruppe $gg wurde zur DomÃ¤nenlokalen Gruppe $dlGroup hinzugefÃ¼gt."
         } else {
-            Write-Output "Globalgruppe $gg ist bereits Mitglied der Domänenlokalen Gruppe $dlGroup."
+            Write-Output "Globalgruppe $gg ist bereits Mitglied der DomÃ¤nenlokalen Gruppe $dlGroup."
         }
     }
 
-    [System.Windows.Forms.MessageBox]::Show("Globalgruppe $gg wurde den Domänenlokalen Gruppen $($dl -join ', ') hinzugefügt.")
+    [System.Windows.Forms.MessageBox]::Show("Globalgruppe $gg wurde den Domänenlokalen Gruppe/n $($dl -join ', ') hinzugefügt.")
 })
 $form.Controls.Add($okButton)
 
 # Beenden-Button
 $exitButton = New-Object System.Windows.Forms.Button
 $exitButton.Text = "Beenden"
-$exitButton.Location = New-Object System.Drawing.Point(310, 250)
+$exitButton.Location = New-Object System.Drawing.Point(320, 300)
 $exitButton.Size = New-Object System.Drawing.Size(100, 30)
 $exitButton.Add_Click({
     $form.Close()
